06744cf633e5342b6bf020a9564075f20037840b
megazortti <39544599+megazortti@users.noreply.github.com>
2021-08-12 21:31:30 -0300

    alpha release 0.2.1

--- js/index.js
+++ js/index.js
@@ -2,10 +2,24 @@ var headerSelect = $('.header-select')[0]
 $('#btnDropDown').on('click', ()=>{
     if(headerSelect.style.display=="block"){
         headerSelect.style.display="none";
+        $('#btnDropDown').removeClass("rotated");
+        $('#btnDropDown').addClass("unrotated");
+
     }else{
         $('html,body').scrollTop(0);
         headerSelect.style.display="block";
+        $('#btnDropDown').addClass("rotated");
+        $('#btnDropDown').removeClass("unrotated");
     }
     
+    
     // grid-template-rows: auto 0vh 100vh auto;
+});
+// Scroll Event..
+document.addEventListener('scroll', function (){
+    if(document.body.getBoundingClientRect().top < -20){
+        console.log("Rolou para baixo");
+    }else{
+        console.log("Não rolou para baixo.");
+    }
 });
